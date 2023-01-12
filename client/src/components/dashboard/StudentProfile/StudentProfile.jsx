b458353e304ad024894f2437d8d732fd68da5e83
Pr454th <93435120+Pr454th@users.noreply.github.com>
2023-01-12 07:11:35 +0530

    Update StudentProfile.jsx

--- client/src/components/dashboard/StudentProfile/StudentProfile.jsx
+++ client/src/components/dashboard/StudentProfile/StudentProfile.jsx
@@ -6,7 +6,8 @@ const StudentProfile = () => {
   const [loading, setLoading] = useState(true);
   const [formData, setFormData] = useState(null);
   const [deptData, setDeptData] = useState(null);
-  const rollNo = localStorage.getItem("rollNo") || "202099";
+  const student=JSON.parse(localStorage.getItem("stuser"));
+  const rollNo = student.rollNo;
   const [department, setDepartment] = useState("");
   const [password, setPassword] = useState("");
   const departmentOptions = [
@@ -22,27 +23,28 @@ const StudentProfile = () => {
   const updateDept = (selectionDept) => {
     setDepartment(selectionDept);
   };
-  const submitForm = (e) => {
-    e.preventDefault();
-    console.log("test");
-    axios
-      .put(`/api/studentupdate/${rollNo}`, {
-        ...formData,
-        deptID: department.value,
-        deptName: department.label,
-        password: password,
-      })
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch((err) => {});
-  };
+  const updateDetails = (e) => {};
   useEffect(() => {
     axios
       .get(`api/studentinfo/${rollNo}`)
       .then((res) => {
         setFormData(res.data[0]);
-        setLoading(false);
+
+        axios
+          .get("api/departments")
+          .then((res) => {
+            console.log(res.data.data);
+            res.data.data.forEach((res) => {
+              setDepartment([
+                ...department,
+                { value: res.deptID, name: res.departmentName },
+              ]);
+            });
+            setLoading(false);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
       })
       .catch((err) => {
         console.log("err" + err);
@@ -51,125 +53,125 @@ const StudentProfile = () => {
 
   if (!loading) {
     return (
-      <div className={"m-5 " + styles["profile-container"]}>
+      <div className={styles["profile-container"]}>
+        <div className="container w-75">
         <div className="row">
           <div className="col-md-12 border-right">
             <div className="p-3 py-5">
-              <div className="d-flex justify-content-between align-items-center mb-3">
-                <h4 className="text-center display-4 mx-auto">Profile</h4>
+            <div className="header">
+              <p className="display-1 my-5">Profile</p>
+            </div>
+              <div className="row mt-2 mx-auto">
+                <div className="col-md-10">
+                  <label className="labels my-3">Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Name"
+                    value={formData.name}
+                    onChange={(e) => {
+                      setFormData({ ...formData, name: e.target.value });
+                    }}
+                  />
+                </div>
               </div>
-              <form>
-                <div className="row mt-2 mx-auto">
-                  <div className="col-md-10">
-                    <label className="labels my-3">Name</label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="Name"
-                      value={formData.name}
-                      onChange={(e) => {
-                        setFormData({ ...formData, name: e.target.value });
-                      }}
-                    />
-                  </div>
+              <div className="row mt-3 mx-1">
+                <div className="col-md-10">
+                  <label className="labels my-3">Roll Number</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Roll Number"
+                    value={formData.rollNo}
+                    onChange={(e) => {
+                      setFormData({ ...formData, rollNo: e.target.value });
+                    }}
+                  />
                 </div>
-                <div className="row mt-3">
-                  <div className="col-md-10">
-                    <label className="labels my-3">Roll Number</label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="Roll Number"
-                      value={formData.rollNo}
-                      onChange={(e) => {
-                        setFormData({ ...formData, rollNo: e.target.value });
-                      }}
-                    />
-                  </div>
-                  <div className="col-md-10">
-                    <label className="labels my-3">College Name</label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="College Name"
-                      value={formData.college}
-                      onChange={(e) => {
-                        setFormData({ ...formData, college: e.target.value });
-                      }}
-                    />
-                  </div>
-                  <div className="col-md-10">
-                    <label className="labels my-3">Department</label>
-                    <Select
-                      value={department}
-                      onChange={updateDept}
-                      options={departmentOptions}
-                      defaultValue={departmentOptions[0]}
-                    />
-                  </div>
-                  <div className="col-md-10">
-                    <label className="labels my-3">Year of Passing</label>
-                    <input
-                      type="number"
-                      className="form-control"
-                      placeholder="Year of Passing"
-                      value={formData.yearOfPO}
-                      onChange={(e) => {
-                        setFormData({ ...formData, yearOfPO: e.target.value });
-                      }}
-                    />
-                  </div>
-                  <div className="col-md-10">
-                    <label className="labels my-3">CGPA</label>
-                    <input
-                      type="number"
-                      className="form-control"
-                      placeholder="0"
-                      value={formData.cgpa}
-                      onChange={(e) => {
-                        setFormData({ ...formData, cgpa: e.target.value });
-                      }}
-                    />
-                  </div>
-                  <div className="col-md-10">
-                    <label className="labels my-3">Email ID</label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="Email ID"
-                      value={formData.email}
-                      onChange={(e) => {
-                        setFormData({ ...formData, email: e.target.value });
-                      }}
-                    />
-                  </div>
-                  <div className="col-md-10">
-                    <label className="labels my-3">New Password </label>
-                    <input
-                      type="password"
-                      className="form-control"
-                      placeholder="New Password"
-                      value={password}
-                      onChange={(e) => {
-                        setPassword(e.target.value);
-                        setFormData({ ...formData, password: e.target.value });
-                      }}
-                    />
-                  </div>
+                <div className="col-md-10">
+                  <label className="labels my-3">College Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="College Name"
+                    value={formData.college}
+                    onChange={(e) => {
+                      setFormData({ ...formData, college: e.target.value });
+                    }}
+                  />
+                </div>
+                <div className="col-md-10">
+                  <label className="labels my-3">Department</label>
+                  <Select
+                    value={department}
+                    onChange={updateDept}
+                    options={departmentOptions}
+                    defaultValue={departmentOptions[0]}
+                  />
+                </div>
+                <div className="col-md-10">
+                  <label className="labels my-3">Year of Passing</label>
+                  <input
+                    type="number"
+                    className="form-control"
+                    placeholder="Year of Passing"
+                    value={formData.yearOfPO}
+                    onChange={(e) => {
+                      setFormData({ ...formData, yearOfPO: e.target.value });
+                    }}
+                  />
+                </div>
+                <div className="col-md-10">
+                  <label className="labels my-3">CGPA</label>
+                  <input
+                    type="number"
+                    className="form-control"
+                    placeholder="0"
+                    value={formData.cgpa}
+                    onChange={(e) => {
+                      setFormData({ ...formData, cgpa: e.target.value });
+                    }}
+                  />
+                </div>
+                <div className="col-md-10">
+                  <label className="labels my-3">Email ID</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Email ID"
+                    value={formData.email}
+                    onChange={(e) => {
+                      setFormData({ ...formData, email: e.target.value });
+                    }}
+                  />
+                </div>
+                <div className="col-md-10">
+                  <label className="labels my-3">New Password </label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    placeholder="New Password"
+                    value={password}
+                    onChange={(e) => {
+                      setPassword(password);
+                      setFormData({ ...formData, password: e.target.value });
+                    }}
+                  />
+                </div>
 
-                  <div className="mt-5 text-center">
-                    <button
-                      className={" display-5 p-3 " + styles["btn"]}
-                      type="button"
-                      onClick={(e) => submitForm(e)}
-                    >
-                      Update Profile
-                    </button>
-                  </div>
+                <div className="mt-5 text-center">
+                  <button
+                    className={" display-5 p-3 " + styles["btn"]}
+                    type="button"
+                    onClick={updateDetails()}
+                  >
+                    Update Profile
+                  </button>
                 </div>
-              </form>
+              </div>
             </div>
           </div>
+        </div>
         </div>
       </div>
     );
