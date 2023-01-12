8d2590d2600d02f985d4cb1bdb1080db28d54618
Pr454th <93435120+Pr454th@users.noreply.github.com>
2023-01-12 07:08:21 +0530

    Update Exam.jsx

--- client/src/components/dashboard/Exam/Exam.jsx
+++ client/src/components/dashboard/Exam/Exam.jsx
@@ -1,133 +1,142 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import InputWithLabel from "../../../components/form/InputWithLabel";
 import Select from "react-select";
 import axios from "axios";
 
-const StaffSignup = () => {
+const Exam = () => {
   const navigate = useNavigate();
-  const [Name, setName] = useState("");
-  const [staff, setStaffId] = useState("");
-  const [department, setDepartment] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [error, setError] = useState("");
-  const [deptId, setDeptId] = useState("5060");
-  useEffect(() => {
-    const id = localStorage.getItem("rollNo");
-    if (id) setDeptId(id);
-    axios.get(`/api/deptcourse/${deptId}`).then((res) => {
-      console.log(res.data);
-    });
-  }, []);
-  const validateEmail = (email) => {
-    var re = /\S+@\S+\.\S+/;
-    return re.test(email);
-  };
+  const staff=JSON.parse(localStorage.getItem("sfuser"));
+  console.log(staff);
+  const [examID, setexamID] = useState("");
+  const [staffName, setstaffName] = useState("");
+  const [subjectName, setsubjectName] = useState("");
+  const [subjectCode, setsubjectCode] = useState("");
+  const [examDate, setexamDate] = useState("");
+  const [venue, setVenue] = useState("");
+  const [timing, setTiming] = useState("");
+  const [mode, setMode] = useState("");
+  const [session, setSession] = useState("");
 
-  const updateDept = (selectionDept) => {
-    setDepartment(selectionDept);
-  };
+  const [error, setError] = useState("");
 
   const submitForm = (e) => {
     e.preventDefault();
     setError("");
     if (
-      name === "" ||
-      staffId === "" ||
-      email === "" ||
-      password === "" ||
-      confirmPassword === "" ||
-      department === ""
+      examID === "" ||
+      staffName === "" ||
+      subjectName === "" ||
+      subjectCode === "" ||
+      examDate === "" ||
+      venue === "" ||
+      timing==="" ||
+      mode==="" ||
+      session===""
     ) {
       setError("Fill all the required fields");
       return;
     }
-    if (password !== confirmPassword) {
-      setError("Passwords do not match");
-      return;
-    }
-    if (!validateEmail(email)) {
-      setError("Invalid email");
-      return;
-    }
+    console.log({
+      examID:examID,
+      staffName:staffName,
+      subjectName:subjectName,
+      subjectCode:subjectCode,
+      examDate:examDate,
+      venue:venue,
+      timing:timing,
+      mode:mode,
+      deptID: staff.deptID,
+      session:session,
+      staffID:staff.staffID
+    });
     axios
-      .post("/api/staffsignup", {
-        name,
-        staffID: staffId,
-        email,
-        deptID: department["value"],
-        password,
+      .post("/api/createexam", {
+        examID:examID,
+        staffName:staffName,
+        subjectName:subjectName,
+        subjectCode:subjectCode,
+        examDate:examDate,
+        venue:venue,
+        timing:timing,
+        mode:mode,
+        deptID: staff.deptID,
+        session:session,
+        staffID:staff.staffID
       })
       .then((res) => {
-        localStorage.setItem(
-          "user",
-          JSON.stringify({
-            staffID: staffId,
-            userType: "staff",
-          })
-        );
-        navigate("/auth/verify/staff");
+        console.log("exam created");
       })
       .catch((err) => {
         console.log(err);
       });
   };
 
-  const departmentOptions = [
-    { value: "AERO", label: "AERO" },
-    { value: "AUTO", label: "AUTO" },
-    { value: "ECE", label: "ECE" },
-    { value: "EIE", label: "EIE" },
-    { value: "CT", label: "CT" },
-    { value: "IT", label: "IT" },
-    { value: "PT", label: "PT" },
-    { value: "RPT", label: "RPT" },
-  ];
-  const [startDate, setStartDate] = useState(new Date());
   return (
     <div className="container">
       <div className="mx-auto border rounded p-3 my-3 w-50">
-        <div className="display-6 my-3">Create Exam</div>
+        <div className="display-6 my-3">Exam</div>
         <form>
           <InputWithLabel
-            label="Subject Name"
+            label="Exam ID"
             inputType="text"
-            val={name}
-            toUpdate={setName}
+            val={examID}
+            toUpdate={setexamID}
+            required={true}
+          />
+          <InputWithLabel
+            label="Staff Name"
+            val={staffName}
+            toUpdate={setstaffName}
+            inputType="text"
+            required={true}
+          />
+          <InputWithLabel
+            label="Subject Name"
+            val={subjectName}
+            toUpdate={setsubjectName}
+            inputType="text"
             required={true}
           />
           <InputWithLabel
             label="Subject Code"
-            val={staffId}
-            toUpdate={setStaffId}
+            val={subjectCode}
+            toUpdate={setsubjectCode}
             inputType="text"
             required={true}
           />
-          <div className="mb-3">
-            <label className="form-label">Department</label>
-            <Select
-              value={department}
-              onChange={updateDept}
-              options={departmentOptions}
-            />
-          </div>
-          <DatePicker
-            selected={startDate}
-            onChange={(date) => setStartDate(date)}
-          />
           <InputWithLabel
-            label="Password"
-            val={password}
-            toUpdate={setPassword}
-            inputType="password"
+            label="Exam Date(DD/MM/YYYY)"
+            val={examDate}
+            toUpdate={setexamDate}
+            inputType="text"
             required={true}
           />
           <InputWithLabel
             label="Venue"
-            val={confirmPassword}
-            toUpdate={setConfirmPassword}
+            val={venue}
+            toUpdate={setVenue}
+            inputType="text"
+            required={true}
+          />
+          <InputWithLabel
+            label="Session"
+            val={session}
+            toUpdate={setSession}
+            inputType="text"
+            required={true}
+          />
+          <InputWithLabel
+            label="Timing"
+            val={timing}
+            toUpdate={setTiming}
+            inputType="text"
+            required={true}
+          />
+          <InputWithLabel
+            label="Mode"
+            val={mode}
+            toUpdate={setMode}
             inputType="text"
             required={true}
           />
@@ -136,21 +145,12 @@ const StaffSignup = () => {
           )}
           <div className="mb-3">
             <button onClick={(e) => submitForm(e)} className="btn btn-primary">
-              Sign Up
+              Create
             </button>
-          </div>
-          <div>
-            <div className="small">
-              Already have an account? <Link to="/auth/login/staff">Login</Link>
-            </div>
-            <div className="small">
-              Not a staff?{" "}
-              <Link to="/auth/signup/student">Student sign up</Link>
-            </div>
           </div>
         </form>
       </div>
     </div>
   );
 };
-export default StaffSignup;
+export default Exam;
